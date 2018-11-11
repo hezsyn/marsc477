347481a3321cc2429b3e1b79f1f819cbead842a3
Brandon <jebster1988@hotmail.com>
2018-11-10 18:10:03 -0800

    Login function working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,22 @@
 class SessionsController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+    user = User.find_by(user_name: params[:user_name])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_url, notice: 'Logged in!'
+    else
+      redirect_to root_path, alert: 'Incorrect log in credentials'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path, notice: 'Logged out!'
+  end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,19 @@ module ApplicationHelper
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   
+	def flash_class(level)
+	    case level
+	        when :notice
+	        	"alert alert-info"
+	        when :success 
+				"alert alert-success"
+	        when :error 
+	        	"alert alert-danger"
+	        when :alert 
+	        	"alert alert-danger"
+	        else
+	        	""
+	    end
+	end
+
 end
